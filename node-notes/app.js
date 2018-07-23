a32a5894fe59d810c2868d44717eaabc4dc94b4b
RudyRed <willgross94@gmail.com>
2018-07-23 16:11:26 -0400

    013 Completed

--- node-notes/app.js
+++ node-notes/app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+const command = process.argv[2];
+// console.log(process.argv);
+console.log(`Command: ${command}`);
+
+if (command === 'add') {
+  console.log('adding new note');
+} else if (command === 'remove') {
+  console.log('Removing Note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading note');
+} else {
+  console.log('command not recognized');
+}
