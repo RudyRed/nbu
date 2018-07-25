053fc8ae469b85f5088d86f66cd9c556339df50b
RudyRed <willgross94@gmail.com>
2018-07-25 16:10:57 -0400

    034 Complete

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,41 @@
 const yargs = require('yargs');
+require('dotenv').config();
+//
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true,
+//     },
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+// 'https://api.darksky.net/forecast/[key]/[latitude],[longitude]'
 
-const geocode = require('./geocode/geocode');
+const request = require('request');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true,
-    },
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// const cb = (error, )
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+request({
+  url: `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/43.86985980000001,-79.4557975`,
+  json: true,
+}, (error, response, body) => {
+  if (!error && response.statusCode == 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather');
   }
 });
