921eaf7310fa474956a1f02a0f31ec5b144cb8ca
RudyRed <willgross94@gmail.com>
2018-07-25 14:42:24 -0400

    031 Complete

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,21 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
   json: true,
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
