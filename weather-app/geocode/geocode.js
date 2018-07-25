fc8823528a7dcff7f2b7b837b0a36f4db49d9da3
RudyRed <willgross94@gmail.com>
2018-07-25 15:15:08 -0400

    033 Complete

---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+const geocodeAddress = (address, cb) => {
+  const encodedAdress = encodeURIComponent(address);
+
+  request({
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAdress}`,
+    json: true,
+  }, (error, response, body) => {
+
+    if (error) {
+      cb('Unable to connect to Google servers');
+    } else if (body.status === 'ZERO_RESULTS') {
+      cb('Unable to find address');
+    } else if (body.status === 'OK') {
+
+      cb(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitud: body.results[0].geometry.location.lng,
+      });
+
+    }
+
+  });
+
+}
+
+module.exports = {
+  geocodeAddress,
+}
