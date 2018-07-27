04fd4c0934126cdd63dc0729a5625a4395ee60b8
RudyRed <willgross94@gmail.com>
2018-07-27 14:57:53 -0400

    044 done

--- nodeWebServer/server.js
+++ nodeWebServer/server.js
@@ -1,33 +1,40 @@
 const express = require('express');
 const hbs = require('hbs');
 
-const app = express();
+var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
-    welcomeMessage: 'Hello welcome',
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
+    pageTitle: 'About Page'
   });
 });
 
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
-    errorMessage: 'uh oh',
+    errorMessage: 'Unable to handle request'
   });
 });
 
-
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('Server is up on port 3000');
 });
