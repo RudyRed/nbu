02a22a8864c7f06bb6d743439dfb778324360eac
RudyRed <willgross94@gmail.com>
2018-07-27 16:21:16 -0400

    046 done

--- nodeWebServer/server.js
+++ nodeWebServer/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now} ${req.method} ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n', e => {
+    if (e) console.log('Unable to append to server.log');
+  });
+
+  console.log(log);
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
