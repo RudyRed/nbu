50afeb73a7a9605b50ba49279a8be790d11b9f0d
RudyRed <willgross94@gmail.com>
2018-07-29 17:05:11 -0400

    053 done

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,35 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
-  const res = utils.add(33, 11);
-
-  if (res !== 44) {
-    throw new Error(`Expected 44 but got ${res}`);
-  }
+  expect(utils.add(33, 11)).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
-  const res = utils.square(12);
-
-  if (res !== 144) {
-    throw new Error(`Expected 144 but got ${res}`);
-  }
+  expect(utils.square(12)).toBe(144).toBeA('number');
 });
+
+
+it('should set firstName and lastName', () => {
+  var user = {location: 'Canada', age: 25};
+  var res = utils.setName(user, 'Andrew Mead');
+
+  expect(res).toInclude({
+    firstName: 'Andrew',
+    lastName: 'Mead'
+  });
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(12);
+//   // expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+//   // expect([2,3,4]).toExclude(1);
+//   expect({
+//     name: 'Andrew',
+//     age: 25,
+//     location: 'Canada'
+//   }).toExclude({
+//     age: 23
+//   })
+// });
